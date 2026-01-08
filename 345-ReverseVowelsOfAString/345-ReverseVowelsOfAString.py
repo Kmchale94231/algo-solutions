793bdc8620512f69b540d3b016aae39915e6a947
Kai McHale <Kaimchale@icloud.com>
2026-01-08 11:32:35 -0600

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.py
@@ -0,0 +1,18 @@
+# Last updated: 1/8/2026, 11:32:35 AM
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels = ("aeiouAEIOU")
+        chars = list(s)
+        l, r = 0, len(chars) - 1
+
+        while l < r:
+            while l < r and chars[l] not in vowels:
+                l += 1
+            while l < r and chars[r] not in vowels:
+                r -= 1
+            
+            chars[l], chars[r] = chars[r], chars[l]
+            l += 1
+            r -= 1
+                
+        return "".join(chars)      
