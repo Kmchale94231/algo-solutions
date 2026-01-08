0f794809e65c18eb2b650f224c1f4c032da381a6
Kai McHale <Kaimchale@icloud.com>
2026-01-08 12:11:50 -0600

    File updated at 1/8/2026, 12:11:50 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.py
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.py
@@ -1,24 +1,5 @@
-# Last updated: 1/8/2026, 12:08:46 PM
+# Last updated: 1/8/2026, 12:11:50 PM
 1class Solution(object):
-2    def reverseVowels(self, s):
-3        vowels = "aeiouAEIOU"
-4        string = list(s)
-5        l, r = 0, len(string) - 1 
-6
-7        while l < r:
-8            if string[l] in vowels and string[r] in vowels:
-9                string[l], string[r] = string[r], string[l]
-10                l += 1
-11                r -= 1
-12            elif string[l] not in vowels:
-13                l += 1
-14            elif string[r] not in vowels:
-15                r -= 1
-16        return "".join(string) 
-17            
-18
-19
-20            
-21
-22            
-23        
+2    def reverseWords(self, s):
+3        return " ".join(s.split()[::-1])
+4        
